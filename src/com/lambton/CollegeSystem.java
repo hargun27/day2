99bf6fe4f304e7099776c7c42bfeb55cceb3c16f
macstudent <hargunmarya2726@gmail.com>
2019-05-23 19:35:26 -0400

    [Hargun] printed name 10 times

--- src/com/lambton/CollegeSystem.java
+++ src/com/lambton/CollegeSystem.java
@@ -1,4 +1,8 @@
 package com.lambton;
 
 public class CollegeSystem {
+    public static void main(String[] args) {
+        System.out.println("hello java");
+        for (int i = 1; i <= 10; i++) System.out.println("hargun");
+    }
 }
